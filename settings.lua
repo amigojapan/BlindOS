d7532c4aecbf8720f5d8c5e87718d0cb69614489
Usmar A Padow <usmpadow@gmail.com>
2015-01-07 16:40:05 +0900

    set back the settings for other people other than frost to default

--- settings.lua
+++ settings.lua
@@ -1,4 +1,4 @@
 pathToSpeachSynth = "~/Downloads/espeak-1.45.04-OSX/espeak-1.45.04/speak"
 parametersToSpeachSynth=""
 advacedMode=false--when advaced mdoe is set to true, it won't say "Y for yes N for no", you just have to know, it will be less verbose in general
-speachSynthOn=false--DIsable this if you are going ot use a screen reader instead of espeak
+speachSynthOn=true--DIsable this if you are going ot use a screen reader instead of espeak
