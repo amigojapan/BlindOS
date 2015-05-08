1a492bd77c46f3c31038515fe916745b837f435b
Usmar A Padow <usmpadow@gmail.com>
2015-05-08 11:57:11 +0900

    Started BBS., our temporarily the pronunciations of Japaese in settings.lua

--- settings.lua
+++ settings.lua
@@ -2,3 +2,19 @@ pathToSpeachSynth = "~/Downloads/espeak-1.45.04-OSX/espeak-1.45.04/speak"
 parametersToSpeachSynth=""
 advacedMode=false--when advaced mdoe is set to true, it won't say "Y for yes N for no", you just have to know, it will be less verbose in general
 speachSynthOn=true--DIsable this if you are going ot use a screen reader instead of espeak
+--./speak -v es-mx "nagai bunshoo dato, wake wakaranai koto ni naru no kaná"
+--./speak -v en --ipa "[[a,i:,u,e,o]]"--あいうえお
+--./speak -v es-mx --ipa "[[ka,ki:,ku,ke,ko]]"--かきくけこ
+--./speak -v es-mx --ipa "[[sa,Si:,su,se,so]]"--さしすせそ
+--./speak -v es-mx --ipa "[[ta,tSi:,tsu,te,to]]"
+--./speak -v es-mx --ipa "[[na ni: nu ne no]]"
+--./speak -v es-mx --ipa "[[ha hi: hu he ho]]"
+--./speak -v es-mx --ipa "[[ma mi: mu me mo]]"
+--./speak -v es-mx --ipa "[[ja  ju  jo]]"
+--japanese rarirurero is difficult it is like the spanish word floruro flɔɾˈuɾɔ how do I type this in espeak phonems?  
+--./speak -v es-mx --ipa "[[wa o n]]"
+--./speak -v en --ipa "[[,oUke͡Ih'i@j@g,oU]]"
+--./speak -v en --ipa "dehncheeh[[dZi:]]sho"
+--./speak -v en --ipa "[[]]@,i:,u:,É,0]]"
+
+--http://espeak.sourceforge.net/phonemes.html
